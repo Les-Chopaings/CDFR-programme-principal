fbdac042fb00528005c3bc4fb2e175aa1616ee40
Guillaume Dalle <guillaume.dalle74@gmail.com>
2026-04-11 17:48:46 +0200

    add main part

--- include/utils.h
+++ include/utils.h
@@ -12,7 +12,7 @@ typedef enum {
     YELLOW
 } colorTeam_t;
 
-inline unsigned long _millis() {
+inline unsigned long millis() {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts); // Utilisation de CLOCK_MONOTONIC pour éviter les changements d'horloge
     return (ts.tv_sec * 1000) + (ts.tv_nsec / 1000000); // Conversion en millisecondes
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "lidar.hpp"
 #include "Arduino.hpp"
 #include "asserv/asserv.h"
+#include "utils.h"
 
 #define SIZEDATALIDAR 10000
 
@@ -17,6 +18,30 @@ bool ctrl_z_pressed = false;
 void ctrlz(int signal) {
     ctrl_z_pressed = true;
 }
+
+enum class MainState{
+    INIT,
+    INITIALIZE,
+    SETHOME,
+    WAITSTART,
+    START,
+    RUN,
+    RETURNHOME,
+    FIN,
+    STOP
+};
+
+void stopAndRest(Asserv asserv, Arduino arduino){
+    asserv.stop();
+    asserv.set_coordinates(0,0,0);
+    asserv.set_motor_state(false);
+    asserv.set_brake_state(true);
+    arduino.controlePompe(pompe::pompe1,0);
+    arduino.controlePompe(pompe::pompe2,0);
+    arduino.controlePompe(pompe::pompe3,0);
+    arduino.controlePompe(pompe::pompe4,0);
+    arduino.stepperEnable(false);
+};
 
 
 int main(int argc, char *argv[]) {
@@ -33,25 +58,30 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 #endif
-
+    bool prev_collide = false;
+    lidarAnalize_t lidarData[SIZEDATALIDAR];
 
     signal(SIGINT, ctrlc);
     signal(SIGTERM, ctrlc);
     signal(SIGTSTP, ctrlz);
+    usleep(100000);
+    MainState currentState = MainState::INIT;
+    MainState nextState = MainState::INIT;
+    bool initStat = true;
 
-    lidarAnalize_t lidarData[SIZEDATALIDAR];
-    bool prev_collide = false;
+    // TEST
+    // asserv.go_to_point(1000, 0, 0);
 
     while (1) {
 
         LOG_SCOPE("Main");
 
-
         int count = SIZEDATALIDAR;
         if(getlidarData(lidarData,count)){
-            int x, y, teta;
+            int16_t x, y, teta;
             int distance;
-            //robotI2C->getCoords(x,y,teta);
+            asserv.get_coordinates(x, y, teta);
+            LOG_DEBUG("xyt : ",x," ",y," ",teta);
             x = 0; y = 0; teta = 0;
             position_t position = {x,y,teta,0};
             rotateLidarData(lidarData, count, -45);
@@ -69,19 +99,113 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        usleep(10000);
+        long unsigned int endTimestamp;
+        switch (currentState) {
+            //****************************************************************
+            // wait to start the initialisation process
+            case MainState::INIT:{
+                if(initStat){
+                    LOG_STATE("INIT");
+                    //TODO
+                }
+                if(arduino.readButton(button::magnet)){
+                    nextState = MainState::INITIALIZE;
+                }
+                break;
+            }
+            //****************************************************************
+            // initialisation first step : enable all actuator
+            case MainState::INITIALIZE:{
+                if(initStat){
+                    LOG_STATE("INITIALIZE");
+                    bool colorTeam = arduino.readButton(button::color);
+                    asserv.set_motor_state(true);
+                    asserv.set_brake_state(false);
+                    asserv.set_coordinates(0,0,0);
+                    arduino.stepperEnable(true);
+                }
+                nextState = MainState::SETHOME;
+                break;
+            }
+            //****************************************************************
+            // initialisation second step : set home
+            case MainState::SETHOME:{
+                if(initStat){
+                     LOG_STATE("SETHOME");
+                    //TODO
+                }
+                nextState = MainState::SETHOME;
+                break;
+            }
+            //****************************************************************
+            // wait start from magnet
+            case MainState::WAITSTART:{
+                if(initStat){
+                    LOG_STATE("WAITSTART");
+                }
+                if(!arduino.readButton(button::magnet)){
+                    nextState = MainState::START;
+                }
+                break;
+            }
+            //****************************************************************
+            // enable all system and start timer
+            case MainState::START:{
+                if(initStat){
+                    LOG_STATE("START");
+                }
+                endTimestamp = millis() + 100000;
+                nextState = MainState::RUN;
+                break;
+            }
+            //****************************************************************
+            // action to play during 100s
+            case MainState::RUN:{
+                if(initStat) LOG_STATE("RUN");
+                bool finish;
+                // TODO
+                if(endTimestamp < millis() && finish){
+                    LOG_GREEN_INFO("END BY TIMER");
+                    nextState = MainState::FIN;
+                }
+                break;
+            }
+            //****************************************************************
+            // disable all actuator
+            case MainState::FIN:
+                if(initStat){
+                    LOG_STATE("FIN");
+                }
+                nextState = MainState::STOP;
+                break;
+            //****************************************************************
+            // END!!
+            case MainState::STOP:
+                if(initStat){
+                    LOG_STATE("STOP");
+                }
+                break;
+            //****************************************************************
+            default:
+                LOG_STATE("default");
+                nextState = MainState::STOP;
+                break;
+        }
 
+        initStat = false;
+        if(currentState != nextState){
+            initStat = true;
+        }
+        currentState = nextState;
 
         if (ctrl_c_pressed){
             break;
         }
+
+        usleep(10000);
     }
 
     LOG_DEBUG("PROCESS KILL");
-    arduino.controlePompe(pompe::pompe1,0);
-    arduino.controlePompe(pompe::pompe2,0);
-    arduino.controlePompe(pompe::pompe3,0);
-    arduino.controlePompe(pompe::pompe4,0);
     lidarStop();
 
     return 0;
