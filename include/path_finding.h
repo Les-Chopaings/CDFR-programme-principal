f73bf05edab087674f45385cc709c62a6c612cdf
Victor Roux Sibillon <victor.roux-sibillon@phelma.grenoble-inp.fr>
2026-04-13 21:06:02 +0200

    fix+feat: path distance + edges toggle (manual + rectangle) + remove coaxial edges toggle

--- include/path_finding.h
+++ include/path_finding.h
@@ -22,6 +22,7 @@ typedef struct {
 
 typedef struct {
     std::vector<pose_t> v;
+    float length;
 } path_t;
 
 typedef struct {
@@ -33,6 +34,7 @@ typedef struct {
     uint8_t start_node_id;
     uint8_t end_node_id;
     float distance;
+    bool enabled;
 } edge_t;
 
 struct a_star_node {
@@ -57,7 +59,15 @@ class PathFindingMap {
 public:
     PathFindingMap();
 
-    node_t *find_closest_node_to_point(pose_t target_point);
+    void update_base_map();
 
-    path_t find_path_between_points(pose_t start_point, pose_t end_point);
+    void enable_all_edges();
+
+    std::vector<edge_t> found_edges_intersecting_rectangle(float rx, float ry, float rw, float rh);
+
+    bool toggle_edge_between_two_nodes(uint8_t node1_id, uint8_t node2_id, bool enabled);
+
+    node_t *find_closest_node_to_point(pose_t target_pose);
+
+    path_t find_path_between_points(pose_t start_point, pose_t end_point, bool remove_coax_nodes = false);
 };
