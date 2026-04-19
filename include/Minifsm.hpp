ed61cc27ea26e9259cf9949bf3426bcd8363228a
NathM07 <jforsuis@gmail.com>
2026-04-19 18:47:48 +0200

    Sorting nuts function done; todo : finish reset (rotate servo to 0 after sorting)

--- include/Minifsm.hpp
+++ include/Minifsm.hpp
@@ -2,8 +2,9 @@
 
 #include "logger.hpp"
 #include "utils.h"
-
-
+#include "Arduino.hpp"
+#define TIMESLIDER 5000
+#define TIMEROTATE 5000
 class Minifsm
 {
 private:
@@ -11,6 +12,9 @@ private:
     int T_start;
     int startSeconds;
     bool toCopy;
+    bool toReset;
+    int servoToMove[4] = {0, 180, 180, 180};
+    int servoCurrentPos[4] = {90, 90, 90, 90};
     bool rotCopied[4] = {0, 0, 0, 0};
     enum class State{
         rotate1,
@@ -18,14 +22,20 @@ private:
         rotate3,
         rotate4,
         idle,
-        toProcess};
+        toProcess,
+        movement,
+        wait,
+        rotwait,
+        init
+    };
+    State followupState = State::toProcess;
     State currentState = State::idle;
 
 
 public:
     Minifsm(/* args */);
     ~Minifsm();
-    int TriNoisette(bool* rotate);
+    int TriNoisette(bool* rotate, Arduino* arduino);
     int MoveUp();
     int MoveDown();
 
